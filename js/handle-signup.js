28b9218f3ab96992efa25614bde1cb69381adb7a
Thomas Groutars <tgroutars@gmail.com>
2017-10-25 19:53:35 +0200

    Send signup to app.caffee.io

---
+++ js/handle-signup.js
@@ -0,0 +1,29 @@
+(function() {
+
+function sendData(url, email, cb) {
+  var XHR = new XMLHttpRequest();
+  var urlEncodedData = 'email=' + encodeURIComponent(email);
+  if (typeof cb === 'function') {
+    XHR.addEventListener('load', cb);
+  }
+  XHR.open('POST', url);
+  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  XHR.send(urlEncodedData);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var forms = document.querySelectorAll('.signup-form');
+  for (var i = 0; i < forms.length; i++) {
+    forms[i].addEventListener('submit', function(evt) {
+      evt.preventDefault();
+      var url = this.action;
+      var input = this.querySelector('input[name="email"]');
+      var email = input.value;
+      sendData(url, email, function() {
+        console.log('sent!');
+      });
+    })
+  }
+}, false);
+
+}())
