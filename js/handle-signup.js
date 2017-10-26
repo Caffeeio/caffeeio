f217b58ad4308a01c7e2882dfc17b980d85a3e4b
Thomas Groutars <tgroutars@gmail.com>
2017-10-26 09:26:40 +0200

    Not an arrow function :facepalm:

--- js/handle-signup.js
+++ js/handle-signup.js
@@ -15,12 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
   var forms = document.querySelectorAll('.signup-form');
   for (var i = 0; i < forms.length; i++) {
     forms[i].addEventListener('submit', function(evt) {
+      var form = this;
       evt.preventDefault();
-      var url = this.action;
-      var input = this.querySelector('input[name="email"]');
+      var url = form.action;
+      var input = form.querySelector('input[name="email"]');
       var email = input.value;
       sendData(url, email, function() {
-        this.className += ' signup-success';
+        form.className += ' signup-success';
       });
     })
   }
